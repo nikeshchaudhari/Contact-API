08854d7a33e1611c39bdb3860712159c6310dc70
Nikesh Chaudhari <info.nikesh12@gmail.com>
2025-02-08 22:31:26 +0545

    Api

--- app.js
+++ app.js
@@ -3,13 +3,14 @@ const app = express();
 const contactRoute = require("./routes/contact");
 const UserRoute = require('./routes/user')
 const bodyparser = require("body-parser");
+const fileUpload = require('express-fileupload')
 const mongoose = require("mongoose");
+
+require('dotenv').config()
 
 const dbConnect = async () => {
   try {
-    await mongoose.connect(
-      "mongodb+srv://Admin:System123@test.nsvf4.mongodb.net/?retryWrites=true&w=majority&appName=Test"
-    );
+    await mongoose.connect(process.env.MONGODB_URI);
     console.log("Database Connected...");
   } catch (err) {
     console.log("something wrong");
@@ -24,7 +25,10 @@ app.use(
   })
 );
 app.use(bodyparser.json());
-
+app.use(fileUpload({
+  useTempFiles : true,
+  // tempFileDir : '/tmp/'
+}));
 app.use("/contact", contactRoute);
 app.use('/auth',UserRoute)
 
